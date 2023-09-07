ce9305b65bed402e072868664f1bac26714ccc3d
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2023-09-07 15:56:45 +0530

    Add files via upload

---
+++ seccond-code/src/basic01.java
@@ -0,0 +1,22 @@
+ class basic {
+    public static void main(String[] args) {
+        int a = 10;
+//        if
+        if ( a == 10){
+            System.out.println("hello anshul ");
+
+
+//            while loop
+
+//            int count = 1;
+//            while (count != 5){
+//                System.out.println(count);
+//                count++;
+//            }
+//            for loop
+            for (int count =1;  count !=5; count++){
+                System.out.println(count);
+            }
+        }
+    }
+}
